01f34b80738de6ad4496f3c221cca11860be27d5
jonreast <joneast@gmail.com>
2018-01-16 20:03:00 -0800

    update to support Azure app services

--- Program.cs
+++ Program.cs
@@ -14,12 +14,23 @@ namespace TodoApi
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .UseAzureAppServices()
+                .Build();
+ 
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+            .UseStartup<Startup>()
+            .Build();
     }
 }
